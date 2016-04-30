3dbceddc4b41cc392bc1a45dfd95b4d061a6a9a1
doxterpepper <dockconnellyoneal@gmail.com>
2016-04-30 19:17:31 -0400

    Making commit before merging changes from my other computer

--- Main.py
+++ Main.py
@@ -2,6 +2,19 @@ import Agent
 import World
 import Gui
 import sys
+import threading
+
+# This keeps the animation loop from blocking the slider.
+# Otherwise when the action method reaches the sleep
+# timer, it blocks everything and the slider can't be
+# moved. This method get's its own thread of execution.
+def actionLoop(agent):
+	try:
+		while True:
+			agent.takeAction()
+	except KeyboardInterrupt:
+		agent.save()
+		exit(0)
 
 if len(sys.argv) < 6:
 	print("Required arguments: save file, alpha, gamma, lambda, Epsilon decay")
@@ -14,9 +27,7 @@ ed = float(sys.argv[5])
 world = World.loadWorld('world.txt')
 gui = Gui.gui(400, world)
 agent = Agent.Agent(world, gui, fn, a, g, l, ed) # alpha, gamma, lambda, Epsilon Decay
-try:
-	while True:
-		agent.takeAction()
-except KeyboardInterrupt:
-	print("Exiting")
-	agent.save()
+
+t = threading.Thread(target=actionLoop, args=[agent])
+t.start()
+gui.window.mainloop()
